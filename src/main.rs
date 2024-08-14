7e260407b46b8292fcea7ffe5de003665b201e7e
ardririy <ardririy@ardririynoMacBook-Air.local>
2024-08-15 01:27:34 +0900

    prepare

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{env, process::exit};
+
 fn main() {
-    println!("Hello, world!");
+    let args = match get_args() {
+        Ok(v) => v,
+        Err(msg) => {panic!("{}", msg);}
+    };
+
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+    println!("main:");
+
+
+}
+
+fn get_args() -> Result<Vec<String>, String> {
+    let args :Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        return Err(String::from("引数の個数が正しくありません"));
+    }
+    return Ok(args);
 }
