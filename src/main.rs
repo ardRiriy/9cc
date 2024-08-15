e8eca50542e5e5100635b840330075e9867ec1d4
ardririy <hynnkgw27@gmail.com>
2024-08-16 04:28:26 +0900

    式を処理できるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,53 @@
 use std::env;
 
 fn main() {
-    let args = match get_args() {
-        Ok(v) => v,
+    let p = match get_args() {
+        Ok(v) => v[1].chars().collect::<Vec<char>>(),
         Err(msg) => {panic!("{}", msg);}
     };
 
     println!(".intel_syntax noprefix");
     println!(".globl _main");
     println!("_main:");
-    println!("  mov rax, {}", args[1]);
+
+    // 最初の項を処理
+    let mut idx = 0;
+    println!("  mov rax, {}", strtol(&p, &mut idx));
+
+    while idx < p.len() {
+        match p[idx] {
+            '+' => {
+                idx += 1;
+                println!("  add rax, {}", strtol(&p, &mut idx));
+            },
+            '-' => {
+                idx += 1;
+                println!("  sub rax, {}", strtol(&p, &mut idx));
+            },
+            _ => {
+                panic!("予期しない文字です: '{}'", p[idx]);
+            }
+        }
+    }
+
     println!("  ret");
+}
+
+// p[idx]から違う記号が出てくるまでを数字として返す
+fn strtol(p: &Vec<char>, idx: &mut usize) -> i32 {
+    let mut num = 0;
+    while *idx < p.len() {
+        match p[*idx] {
+            '0'..='9' => {
+                num = num * 10 + (p[*idx] as i32 - '0' as i32);
+            },
+            _ => {
+                break;
+            }
+        }
+        *idx += 1;
+    }
+    num
 }
 
 fn get_args() -> Result<Vec<String>, String> {
